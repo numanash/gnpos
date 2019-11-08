f6d6973f68cc46d459e9fac993142006a6e588bd
Numan Ashiq <48905195+numanash@users.noreply.github.com>
2019-11-08 17:00:28 +0500

    updations from the office

---
+++ client/app/components/Layout/Navbar/NavbarLeft/Index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Aux from '../../../../constants/hoc/_Aux';
+
+
+class NavbarLeft extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (<Aux>
+            <div></div>
+        </Aux>);
+    }
+}
+
+export default NavbarLeft;
